a5029c284a3356ba2366d74ad87077e238b720b0
Vlad <vladkochetov591@gmail.com>
2025-06-28 10:15:34 +0200

    ugadai chislo

--- ugadai chislo.cpp
+++ ugadai chislo.cpp
@@ -8,25 +8,25 @@ int main()  {
         int difficulty;
         int randNumber = 0;
         int userGuess = 0;
-         cout << "1. EASY (1/2) 2. NORMAL (10) 3. HARD (100) 4. IMPOSIBLE (10000) 5. Exit\n";
+        cout << "1. EASY (1/2) 2. NORMAL (10) 3. HARD (100) 4. IMPOSIBLE (10000) 5. Exit\n";
         cin >> difficulty;
 
         switch (difficulty) {
         case 1:
-             srand(time(0));
-             int randomNum = rand() % 3;
+            srand(time(0));
+            randNumber = rand() % 2 + 1;
             break;
         case 2:
             srand(time(0));
-            int randomNum = rand() % 11;
+            randNumber = rand() % 10 + 1;
             break;
         case 3:
             srand(time(0));
-            int randomNum = rand() % 101;
+            randNumber = rand() % 100 + 1;
             break;
         case 4:
             srand(time(0));
-            int randomNum = rand() % 10001;
+            randNumber = rand() % 10000 + 1;
             break;
         default:
             return 0;
